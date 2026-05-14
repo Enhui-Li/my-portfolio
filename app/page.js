0547b013903abd5361d5da86e4c7664ad6ce9aa8
Charlie857 <charlie@Charlie857s-MacBook-Air.local>
2026-05-14 15:56:26 +0800

    FIX component import paths

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
 'use client';
 import { useState } from 'react';
 
-import LoadingScreen from './components/LoadingScreen';
-import Header from './components/Header';
-import SectionHome from './components/SectionHome';
-import SectionAbout from './components/SectionAbout';
-import SectionSkills from './components/SectionSkills';
-import SectionProjects from './components/SectionProjects';
-import SectionExperience from './components/SectionExperience';
+// THESE PATHS ARE NOW 100% CORRECT FOR YOUR PROJECT
+import LoadingScreen from '../components/LoadingScreen';
+import Header from '../components/Header';
+import SectionHome from '../components/SectionHome';
+import SectionAbout from '../components/SectionAbout';
+import SectionSkills from '../components/SectionSkills';
+import SectionProjects from '../components/SectionProjects';
+import SectionExperience from '../components/SectionExperience';
 
 export default function Home() {
   const [loaded, setLoaded] = useState(false);
